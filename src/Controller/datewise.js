2bd9d39a7a4c1440dadbb84ae394498c9b8ad788
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-03-26 20:18:12 +0530

    added datewise controller file

---
+++ src/Controller/datewise.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const { db } = require("../models/user");
+const Transaction = require("../models/transaction");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await Transaction.aggregate([
+      {
+        $match: {
+          CreatedAt: {
+            $gte: new Date("2022-03-26"),
+            $lte: new Date("2022-03-27"),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $dayOfYear: "$CreatedAt" },
+          totalAmount: { $sum: { $multiply: "$amount" } },
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    return res.status(200).json({
+      total: result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
+});
+module.exports = router;
