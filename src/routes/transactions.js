5c50151e401346f058608d8aafc92359b75d8003
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-03-29 11:58:58 +0530

    added transactions routes file

---
+++ src/routes/transactions.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getTransactions,
+  addTransaction,
+  updateTransaction,
+  deleteTransaction,
+} = require("../Controller/transactions_controller");
+router.route("/").get(getTransactions).post(addTransaction);
+router.route("/:id").put(updateTransaction).delete(deleteTransaction);
+module.exports = router;
