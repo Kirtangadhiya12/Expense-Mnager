b8e86e29f577b29ff218ba998996ab5590d7cf38
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-03-29 11:56:41 +0530

    added monthwise expense controller file

---
+++ src/Controller/monthwiseexpense.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const { db } = require("../models/user");
+const Transaction = require("../models/transaction");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await Transaction.aggregate([
+      {
+        $group: {
+          _id: {
+            month: { $month: "$CreatedAt" },
+          },
+          totalAmount: { $sum: { $multiply: "$amount" } },
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    return res.status(200).json({
+      total: result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+      message: `the error is ${err}`
+    });
+  }
+});
+module.exports = router;
